6eeb89bae3bcd17258c7ff2b401761f308977487
Soumyadip-62 <soumyadippandit2@gmail.com>
2025-06-15 00:47:09 +0530

    added skills icons

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import FooterSection from "./components/Footer";
 import HireMeSection from "./components/HireMesection";
 import ProjectsSection from "./components/Myprojects";
 import ServicesSection from "./components/Services";
+import TechStacks from "./components/TechStacks";
 import WorkExperienceSection from "./components/WorkExperience";
 
 function App() {
   return (
     <>
       <Banner />
+      <TechStacks/>
       <ServicesSection />
       <WorkExperienceSection />
       <ProjectsSection />
---
+++ src/components/TechStacks.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react'
+import htmlLogo from "/assets/images/Logos/html5-without-wordmark-color.svg";
+import cssLogo from "/assets/images/Logos/css-3-seeklogo.com.svg";
+import JsLogo from "/assets/images/Logos/javascript-seeklogo.com.svg";
+import ReactLogo from '/assets/images/Logos/reactjs-icon.svg'
+import NextLogo from "/assets/images/Logos/next-js-seeklogo.com.svg";
+import ExpressLogo from "/assets/images/Logos/express-seeklogo.com.svg";
+import NodeLogo from "/assets/images/Logos/nodejs-seeklogo.com.svg";
+import mongoLogo from "/assets/images/Logos/mongodb-seeklogo.com.svg";
+import bootstrapLogo from "/assets/images/Logos/bootstrap-5-seeklogo.com.svg";
+import npmLogo from "/assets/images/Logos/npm-seeklogo.com-2.svg";
+import githubLogo from "/assets/images/Logos/github-seeklogo.com.svg";
+import tailwindLogo from "/assets/images/Logos/tailwind-css-wordmark-seeklogo.com.svg";
+import tslogo from "/assets/images/Logos/typescript.svg";
+
+const TechStacks = () => {
+  return (
+    <div>
+
+      <img src={htmlLogo} alt="" />
+
+    </div>
+  )
+}
+
+export default TechStacks
